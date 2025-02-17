6e02601ee967931bb7231616768c2e9c79c505e1
qasimovatenzile <qasimovatenzile@gmail.com>
2025-02-17 11:47:43 +0400

    dddss

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  ExportTrailingSlash: true,
+  reactStrictMode: true,
+  swcMinify: true,
   images: {
     unoptimized: true,
   },
