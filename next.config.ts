19cfb3de4cc39e5efb976b1b3ca72a8a9be03ba0
qasimovatenzile <qasimovatenzile@gmail.com>
2025-02-17 20:57:12 +0400

    next

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    unoptimized: true,
+  experimental: {
+    serverActions: true,
   },
 };
 
